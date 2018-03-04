b2803bb3955fceefc3208d5617d6d6f71ef3b35c
ftwdjw <ftwdjw@gmail.com>
2018-03-04 13:48:03 -0600

    works

--- lorentzTransformation/Program.cs
+++ lorentzTransformation/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
 
 namespace lorentzTransformation
 {
     class MainClass
     {
+        public const int N = 100;
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            int i;
+            double v,vSquared,gamma;
+
+            string[] lorentz = new string[N]; 
+
+            for (i = 0; i < N; i++)
+            {
+                v = (double)i / (double)N;//normalilzed to the speed of light C
+                vSquared = Math.Pow(v, 2);
+                gamma = 1 / Math.Sqrt((1 - vSquared));
+
+                lorentz[i] = v.ToString() + "," + gamma.ToString();
+            }
+
+
+            File.WriteAllLines("lorentz.csv", lorentz); 
+            Console.WriteLine("have written file"); 
+            Console.ReadLine();
+
         }
     }
 }
